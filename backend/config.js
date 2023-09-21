fbd876273d350e2666bdd11f287d086a8b0d2429
Nadun <85360173+NadunPabasara@users.noreply.github.com>
2023-09-21 21:13:34 +0530

    Update config.js
    Remove mongo url from config.js file

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5000;
 
 export const mongoDBURL =
-  "mongodb+srv://admin:admin123@book-store-mern.npmym01.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "mongodb url";
