196c8db4ea4d3067e62aeaa353d5b2995dc74b7f
NadunPabasara <nadunpabasara@gmail.com>
2023-09-21 21:03:35 +0530

    Bookstore-MERN first commit

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express, { request, response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Opt 1: Allow all origins with default of cors(*)
+app.use(cors());
+
+// Opt 2: Allow custom origins
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     method: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to the database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
